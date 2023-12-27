28db62ea41f67a764a206198701700bb5345cc5d
MyKl-Y <michaelyyim@gmail.com>
2023-12-26 23:25:29 -0500

    Refactor code

--- entity.py
+++ entity.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import copy
-from typing import Tuple, TypeVar, TYPE_CHECKING
+from typing import Optional, Tuple, TypeVar, TYPE_CHECKING
 
 if TYPE_CHECKING:
     from game_map import GameMap
@@ -13,8 +13,12 @@ class Entity:
     """
     A generic object to represent players, enemies, items, etc.
     """
+
+    gamemap: GameMap
+
     def __init__(
         self,
+        gamemap: Optional[GameMap] = None,
         x: int = 0,
         y: int = 0,
         char: str = "?",
@@ -28,16 +32,33 @@ class Entity:
         self.color = color
         self.name = name
         self.blocks_movement = blocks_movement
+        if gamemap:
+            # If gamemap isn't provided now then it will be set later.
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
-    def spawn(self: T, game_map: GameMap, x: int, y: int) -> T:
+    def spawn(self: T, gamemap: GameMap, x: int, y: int) -> T:
         """
         Spawn a copy of this entity at the given location
         """
         clone = copy.deepcopy(self)
         clone.x = x
         clone.y = y
-        game_map.entities.add(clone)
+        clone.gamemap = gamemap
+        gamemap.entities.add(clone)
         return clone
+    
+    def place(self, x: int, y: int, gamemap: Optional[GameMap] = None) -> None:
+        """
+        Place this entity at a new location. Handles moving across GameMaps.
+        """
+        self.x = x
+        self.y = y
+        if gamemap:
+            if hasattr(self, "gamemap"):
+                self.gamemap.entities.remove(self)
+            self.gamemap = gamemap
+            gamemap.entities.add(self)
 
     def move(self, dx: int, dy: int) -> None:
         """
